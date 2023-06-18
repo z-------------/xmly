cee8f3595ace52f98fbaedae8db5e2d98a737bd5
Zack Guard <zacharyguard@gmail.com>
2023-06-18 14:10:13 +0800

    Add test for overriding builtin parseHook

--- tests/tparsehook.nim
+++ tests/tparsehook.nim
@@ -1,12 +1,16 @@
 import pkg/xmly
 import std/[
   parsexml,
+  strutils,
   times,
   unittest,
 ]
 
 proc parseHook(s: string; dest: var DateTime) =
   dest = times.parse(s, "yyyy-MM-dd'T'hh:mm:ssz")
+
+proc parseHook(s: string; dest: var bool) =
+  dest = not parseBool(s)
 
 test "custom parse hooks":
   type
@@ -16,6 +20,24 @@ test "custom parse hooks":
   const Xml = "<date>2023-06-18T05:13:48Z</date>"
   let expected = Document(
     date: dateTime(2023, mJun, 18, 5, 13, 48, zone = utc()),
+  )
+  let parsed = Document.fromXml(Xml)
+  check parsed == expected
+
+test "override builtin parse hook":
+  type
+    Document = object
+      myBool: seq[bool]
+
+  const Xml = """
+<myBool>false</myBool>
+<myBool>true</myBool>
+<myBool>false</myBool>
+<myBool>false</myBool>
+<myBool>true</myBool>
+  """
+  let expected = Document(
+    myBool: @[true, false, true, true, false],
   )
   let parsed = Document.fromXml(Xml)
   check parsed == expected
